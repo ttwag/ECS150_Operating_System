22d6887269af5b2cd210269936528ad67d7e8b26
ttwag <taowang@ucdavis.edu>
2025-05-28 22:47:41 -0700

    create testing framework

--- project2/part2/apps/queue_tester.c
+++ project2/part2/apps/queue_tester.c
@@ -1,8 +1,26 @@
+#include <assert.h>
 #include <stdio.h>
 #include <queue.h>
 
+/* queue test */
+void test_create() {
+    queue_t qu = queue_create();
+    assert(qu != NULL);
+    assert(queue_length(qu) == 0);
+    queue_destroy(qu);
+}
+
+
+
+void run_test(const char *test_name, void (*test_func)(void)) {
+    printf("Running %s...", test_name);
+    test_func();
+    printf("PASSED\n");
+}
+
 int main() {
-    
-    printf("Hello\n");
+    printf("\n===Begin Queue Unit Test===\n");
+    run_test("QUEUE_CREATE TEST", test_create);
+    printf("All Test Passed\n\n");
     return 0;
 }
